1511a0418f29951486c6dcd19a7bf6467a6dc9dc
dannita09sofia <dannita09sofia@gmail.com>
2025-11-06 18:29:07 +0000

    Que desespero

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const titulo = document.getElementById("titulo");
-
+const textoInicial = document.getElementById("textoInicial");
 let estadoInicial = true;
+let posicionTexto= 0;
+const textos = ["Holis", "bienvenidos", "a", "esta", "pagina"];
+
+textoInicial.innerText = textos[0];
+
+textoInicial.addEventListener("click", () => {
+    if(posicionTexto <textos.lenght - 1) {
+        posicionTexto = posicionTexto + 1;
+        textoInicial.innerText = textos [posicionTexto]
+       
+    } else {
+        posicionTexto = 0
+        textoInicial.innerText =textos[posicionTexto]
+       
+    }
+  
+})
 
 titulo.addEventListener("click", () => {
     if(estadoInicial === true) {
-        titulo.innerText = "Bienvenidos"
+        titulo.innerText = "Bienvenidos";
     } else {
-        titulo.innerText = "Hola soy Aladinn"
+        titulo.innerText = "Hola soy Aladinn";
     }
     estadoInicial = !estadoInicial;
 })
